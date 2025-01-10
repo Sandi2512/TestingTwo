85c0a0ebc00b189cb042b164ca51243c150671fe
sandi2512 <sandytun25@gmail.com>
2025-01-10 22:18:32 +0630

    fourth

--- email.php
+++ email.php
@@ -1,6 +1,7 @@
  <?php
  echo "Another email";
- ehco "Another email sent";
+ echo "Another email sent";
  echo "third line from cli";
 echo "Another email sent";
 echo "third line from online";
+echo "fourth line from cli";
